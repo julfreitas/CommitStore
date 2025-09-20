a03d9357e75babc79ebdda59422afe5619e72361
juliafreitas <juliafreitas20177@gmail.com>
2025-09-20 00:24:47 -0300

    corrige importação do getAuth para o caminho correto

--- app/api/store/is-seller/route.js
+++ app/api/store/is-seller/route.js
@@ -1,4 +1,4 @@
-import { getAuth } from "@/clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
 import authSeller from "@/middlewares/authSeller";
 import { NextResponse } from "next/server";
