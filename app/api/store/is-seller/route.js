6164a85601c9c750cade0fb1b97530509473d21d
juliafreitas <juliafreitas20177@gmail.com>
2025-09-20 00:06:43 -0300

    adiciona rota GET p/ verificar se o user é um seller e retornar informações da loja

---
+++ app/api/store/is-seller/route.js
@@ -0,0 +1,28 @@
+import { getAuth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import authSeller from "@/middlewares/authSeller";
+import { NextResponse } from "next/server";
+
+//verifica se o usuário autenticado é um vendedor(Auth Seller) e, caso seja, retorna informações da loja vinculada a ele
+export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
+    const isSeller = await authSeller(userId);
+
+    if (!isSeller) {
+      return NextResponse.json({ error: "Não autorizado" }, { status: 401 });
+    }
+
+    const storeInfo = await prisma.store.findUnique({
+      where: { userId },
+    });
+
+    return NextResponse.json({ isSeller, storeInfo });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: error.code || error.message },
+      { status: 400 }
+    );
+  }
+}
